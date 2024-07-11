d99e24a796fcde337cb8db2d199324221930294b
Odecius <ofsouzai@gmail.com>
2024-07-11 14:49:53 +0100

    Adicionando !

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ function verificarChute() {
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o número secreto(${numeroSecreto}) com ${tentativas} ${palavraTentativa}!`;
+        let mensagemTentativas = `Você descobriu o número secreto!(${numeroSecreto}) com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         
         document.getElementById('reiniciar').removeAttribute('disabled');
