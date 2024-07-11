296fde60819f9defe837d6c255a5d97769500fa0
Odecius <156013293+Odecius@users.noreply.github.com>
2024-07-11 11:12:19 +0100

    Update app.js
    SCRIPT WITHOUT COMMENTS..

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
 let ListaDeNumerosSorteados = [];
 let numeroLimite = 10;
-
-// era assim dai evoluiu     let titulo = document.querySelector('h1');// NOMEIO TITULO E PECO PARA ELE BUSCAAR A INFORMACAO NO SCIPT HTML NO CAMPO H1
-//<h1></h1> O 1º H1 EH A TaG
-//era assim , evoluiu para funcao    titulo.innerHTML = 'Jogo do número secreto';
-
-//   let paragrafo = document.querySelector('p');// busco a informacao do <p class="texto__paragrafo"></p>
-//    paragrafo.innerHTML = 'Escolha um numero entre 1 e 10';
-
-// verificarChute() eh onde o botao esta e vamos criar uma funcao para ele
-//<button onclick="verificarChute()" class="container__botao">Chutar</button>
-
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-//  inicialmente o codigo estava como abaixo, porem por boa pratica e facilidade na manutencao,
-//  criamos uma funcao para isso:
-//exibirTextoNaTela('h1', 'Jogo do número secreto!');            /// fora da chave chamamos a funcao
-//exibirTextoNaTela('p', 'Escolha um número d 0 a 10:');
-
 function exibirTextoNaTela(tag, texto) {  
-    //dentro da chave apenas as instrucoes
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2} );
 }
 
-
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto!');            /// fora da chave chamamos a funcao
+    exibirTextoNaTela('h1', 'Jogo do número secreto!');           
     exibirTextoNaTela('p', 'Escolha um número de 0 a 10:');
     
 }
@@ -36,17 +18,14 @@ function exibirMensagemInicial() {
 exibirMensagemInicial();
     
 function verificarChute() {
-    let chute = document.querySelector('input').value; //  input vm dai => <input type="number" min="1" max="10" class="container__input">
+    let chute = document.querySelector('input').value; 
     
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto(${numeroSecreto}) com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
-        //para abilitar o botao de novo jogo, precisamos verificar no doc html o campo do botao,
-        // referente  a ele, no caso deste ID, e vamos informar na proxima linha
-        // que queremos que o campo do ID tal faca, no caso remover o atributo
-        // no caso deste Disable:
+        
         document.getElementById('reiniciar').removeAttribute('disabled');
         } else {
                 if (chute > numeroSecreto) {
@@ -58,22 +37,15 @@ function verificarChute() {
                 limparCampo();
         }
 }
-
-//    function gerarNumeroAleatorio() {
-//      return parseInt(Math.random() *10 + 1);    //para mostrarmos os resultado, adiciono Return
-//    
-//}
-//    era assim e atulizamos abaixo pq criamos a lista de numeros ListaDeNumerosSorteados
-//    para que os numeros nao voltassem a sair...
-    
+ 
 function gerarNumeroAleatorio() {
-    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);    //para mostrarmos os resultado, adiciono Return
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);    
     let quantidadeDeElementosNaLista = ListaDeNumerosSorteados.length;
 
-    if (quantidadeDeElementosNaLista == numeroLimite )   //  10 pq eu quero nº de 0 a 10 ai em cima ta 
+    if (quantidadeDeElementosNaLista == numeroLimite )    
     ListaDeNumerosSorteados = []
 
-    if (ListaDeNumerosSorteados.includes(numeroEscolhido)) {   // o includes verifica se o item () esta na lista
+    if (ListaDeNumerosSorteados.includes(numeroEscolhido)) {  
         return gerarNumeroAleatorio();
     } else {
         ListaDeNumerosSorteados.push(numeroEscolhido);
@@ -83,7 +55,7 @@ function gerarNumeroAleatorio() {
 }
 
 function  limparCampo() {
-    chute = document.querySelector('input')  // aqui cordenadas geograficas do lugar que queremos
+    chute = document.querySelector('input')  
     chute.value = '';
     
 }
